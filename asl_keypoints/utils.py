926f8d63767ed4617288ec007457e7784f502243
DRobinson4105 <drobinson4105@gmail.com>
2024-09-28 14:39:27 -0400

    Text to Gloss

---
+++ asl_keypoints/utils.py
@@ -0,0 +1,71 @@
+from langchain_community.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from dotenv import load_dotenv
+
+def text2gloss(text):
+    return conversation_chain.invoke(input=text)['response']
+
+#TODO
+def gloss2video(gloss):
+    pass
+
+#TODO
+def video2pose(video):
+    pass
+
+#TODO
+def pose2vis(pose):
+    pass
+
+def visualize(text):
+    gloss = text2gloss(text)
+    video = gloss2video(gloss)
+    pose = video2pose(video)
+    vis = pose2vis(pose)
+    return vis
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    llm = ChatOpenAI(model_name="gpt-4o-mini")
+
+    custom_prompt = """
+    Human: Here are some examples of translations from English text to ASL gloss:
+    Examples:
+    Apples ==> APPLE
+    you  ==> IX-2P
+    your  ==> IX-2P
+    Love ==> LIKE
+    My ==> IX-1P
+    Thanks ==> THANK-YOU
+    am ==> 
+    and ==> 
+    be ==>
+    of ==>
+    video ==> MOVIE
+    image ==> PICTURE
+    conversations ==> TALK
+    type of ==> TYPE
+    ? ==> QUESTION
+    Watch ==> SEE
+    My name is David ==> MY NAME D-A-V-I-D
+
+    {history}
+    Human: {input}
+    AI: Translate the following English text to ASL Gloss and surround it with tags <gloss> and </gloss>.
+    """
+
+    prompt_template = PromptTemplate(input_variables=['history', 'input'], template=custom_prompt)
+
+    memory = ConversationBufferMemory()
+
+    conversation_chain = ConversationChain(
+        llm=llm,
+        memory=memory,
+        prompt=prompt_template
+    )
+
+    res = text2gloss("How are you?")
+    print(f"How are you? ==> {res}")
